dc5c584653ac593a4b904550dcd87258818b0fd3
SusanaIsabelGuerreiro <susana.isabel.guerreiro95@gmail.com>
2021-11-07 03:18:28 +0000

    allow to reset/start a new game

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  margin-top: 16px;
+  font-size: 16px;
+  color: white;
+  border-radius: 8px;
+  border: none;
+  padding: 8px;
+  width: 120px;
+  background-color: indianred;
+`;
+
+export default Button;
